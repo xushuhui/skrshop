ad88260e49f41a7f0bc6ff7c8a6dd4fbdb8e02c8
xushuhui <474497097@qq.com>
2020-08-19 10:27:58 +0800

    reformat

--- app/request/user.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type Login struct {
-	Phone    string `json:"phone" binding:"required,mobile" comment:"手机号"`
-	Password string `json:"password" binding:"required" comment:"密码"`
-}
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
+/**
+ * Copyright (c) 2019 - xushuhui
+ * Author: xushuhui
+ * 微信公众号: 互联网工程师
+ * Email: xushuhui@qq.com
+ * 博客: https://xushuhui.github.io
+ */
 package main
 
 import (
+	"goal/internal/router"
+	"goal/pkg/core"
 	"os"
 	"os/signal"
-	"skrshop-api/core"
-	"skrshop-api/route"
 	"syscall"
 )
 
 func main() {
+
 	core.StartModule()
 
-	route.HttpServerRun()
+	router.HttpServerRun()
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	route.HttpServerStop()
+	router.HttpServerStop()
+
 }
